f81b77f1ad39f74a51428f55fceca19deefeeb32
Anderson <ander023@163.com>
2024-10-13 21:03:01 +0800

    made dropdown menu interactable

--- javascript.js
+++ javascript.js
@@ -12,7 +12,7 @@ fetch('all_teachers_info.json')
     return response.json();
   })
   .then(data => {
-    console.log(data); // The JSON data will be available here
+    console.log(data);
   })
   .catch(error => {
     console.error('There has been a problem with your fetch operation:', error);
@@ -23,13 +23,11 @@ function updateElements() {
       const container = document.getElementById('form');
 
       if (window.innerWidth < 420) {
-        // Use innerHTML to update the input and button elements
         container.innerHTML = `
           <input id="staffInput" type="text" autofocus placeholder="Enter staff name" autocomplete="off" class="form-control d-inline w-auto" size="15">
           <button id="submitButton" class="btn btn-primary" style="padding: 5px 10px; font-size: 10px; background-color: #008fa0; border: none;">Submit</button>
         `;
       } else {
-        // Revert the innerHTML back to the default (larger screen) state
         container.innerHTML = `
           <input id="staffInput" type="text" autofocus placeholder="Enter staff name" autocomplete="off" class="form-control d-inline w-auto">
           <button id="submitButton" class="btn btn-primary" style="background-color: #008fa0; border: none;">Submit</button>
@@ -37,10 +35,17 @@ function updateElements() {
       }
     }
 
-    // Run the function when the DOM is fully loaded
     document.addEventListener('DOMContentLoaded', updateElements);
-
-    // Add event listener to handle window resizing
     window.addEventListener('resize', updateElements);
 
+
+let dropdownItems = document.querySelectorAll('.dropdown-item');
+let dropdownButton = document.getElementById('dropdownMenuButton');
+
+
+dropdownItems.forEach(item => {
+  item.addEventListener('click', function() {
+      dropdownButton.innerHTML = '&nbsp;&nbsp;' + this.textContent + '&nbsp;&nbsp;';
+  });
+});
 
