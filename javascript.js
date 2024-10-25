5a66f3341d3f21e6156ce05e1170cf324da7a288
Anderson <ander023@163.com>
2024-10-25 18:26:36 +0800

    finished declaration.html

--- javascript.js
+++ javascript.js
@@ -1,22 +1,16 @@
-//https://chatgpt.com/share/1fd464af-c160-4858-9183-17b8eab367b7
-//Mdm Miss Mr Mrs Ms
-//run localhost first using "python: -m http.server"
-//the visit http://localhost:8000 python
-
+//Hello there! This website is developed by Anderson in October 2024!
 
 function updateElements() {
-      const container = document.getElementById('form');
+      let button = document.getElementById('submitButton');
+      let inputBox = document.getElementById('staffInput');
 
       if (window.innerWidth < 420) {
-        container.innerHTML = `
-          <input id="staffInput" type="text" autofocus placeholder="Enter staff name" autocomplete="off" class="form-control d-inline w-auto" size="15">
-          <button id="submitButton" class="btn btn-primary" style="padding: 5px 10px; font-size: 10px; background-color: #008fa0; border: none;">Submit</button>
-        `;
-      } else {
-        container.innerHTML = `
-          <input id="staffInput" type="text" autofocus placeholder="Enter staff name" autocomplete="off" class="form-control d-inline w-auto">
-          <button id="submitButton" class="btn btn-primary" style="background-color: #008fa0; border: none;">Submit</button>
-        `;
+        button.style.padding = "5px 10px";
+        button.style.fontSize = "10px";
+      } 
+
+      if (window.innerWidth < 330) {
+        inputBox.setAttribute("size", "10")
       }
     }
 
